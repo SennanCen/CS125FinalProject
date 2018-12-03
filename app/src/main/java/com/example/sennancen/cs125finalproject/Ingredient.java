a9fa48b29faf1b1276723eb95468a9b6fe1549f2
Sennan Cen <sennanc2@illinois.edu>
2018-12-03 12:43:08 -0600

    Create more activity.

---
+++ app/src/main/java/com/example/sennancen/cs125finalproject/Ingredient.java
@@ -0,0 +1,13 @@
+package com.example.sennancen.cs125finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ingredient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredient);
+    }
+}
--- app/src/main/java/com/example/sennancen/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/sennancen/cs125finalproject/MainActivity.java
@@ -1,30 +1,51 @@
 package com.example.sennancen.cs125finalproject;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 public class MainActivity extends AppCompatActivity {
+    // three button on the main page.
     private Button button ;
-
+    private Button button2;
+    private Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity2();
             }
         });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openIngredient();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTaste();
+            }
+        });
     }
-
+    // functions that opening Location, Ingredient, and Taste.
     public void openActivity2() {
         Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+    public void openIngredient() {
+        Intent intent = new Intent(this, Ingredient.class);
+        startActivity(intent);
+    }
+    public void openTaste() {
+        Intent intent = new Intent(this, Taste.class);
         startActivity(intent);
     }
 }
